e48fb77d2ac92e723c7a6640764563bfe31c22ad
kylem038 <misenckj@gmail.com>
2021-11-15 21:46:02 -0700

    Add types file + show basic grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const gameBoard: GameBoard = {
+    boardHeight: 10,
+    boardWidth: 10,
+  }
+
+  const elements = [];
+  for (let i = 0; i < gameBoard.boardHeight; i++) {
+    elements.push(<div className="grid-cell"></div>);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Battleship</h1>
+      <p>Finding game, place your ships</p>
+      <div id="game-board" className="grid-container">
+        {elements}
+      </div>
     </div>
   );
 }
---
+++ frontend/src/types.d.ts
@@ -0,0 +1,4 @@
+interface GameBoard {
+  boardWidth: number;
+  boardHeight: number;
+}
