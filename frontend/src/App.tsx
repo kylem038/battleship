0aa2ba8e18a177fa0e4befb8da57b3957a18c0c6
kylem038 <misenckj@gmail.com>
2021-11-17 11:39:18 -0700

    Fix logo error + turn board into table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,52 @@ const App = () => {
     boardWidth: 10,
   }
 
-  const elements = [];
-  for (let i = 0; i < gameBoard.boardHeight; i++) {
-    elements.push(<div className="grid-cell"></div>);
+  const generateRow = (rowNum: number) => {
+    const elements = [];
+    for (let i = 0; i < gameBoard.boardHeight; i++) {
+      elements.push(<td key={`${i} - ${rowNum}`} className="grid-cell"></td>);
+    }
+    return elements;
   }
 
   return (
     <div className="App">
       <h1>Battleship</h1>
       <p>Finding game, place your ships</p>
-      <div id="game-board" className="grid-container">
-        {elements}
-      </div>
+      <table id="game-board">
+        <tbody className="grid-container">
+          <tr>
+            {generateRow(0)}
+          </tr>
+          <tr>
+            {generateRow(1)}
+          </tr>
+          <tr>
+            {generateRow(2)}
+          </tr>
+          <tr>
+            {generateRow(3)}
+          </tr>
+          <tr>
+            {generateRow(4)}
+          </tr>
+          <tr>
+            {generateRow(5)}
+          </tr>
+          <tr>
+            {generateRow(6)}
+          </tr>
+          <tr>
+            {generateRow(7)}
+          </tr>
+          <tr>
+            {generateRow(8)}
+          </tr>
+          <tr>
+            {generateRow(9)}
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
