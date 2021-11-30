20e55a51a5bfebe29fc5c16749ec914fd5286aee
kylem038 <misenckj@gmail.com>
2021-11-29 20:12:20 -0700

    Able to set cell height and width

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,22 @@ const App = () => {
     boardWidth: 10,
   };
 
-  const generateRow = (rowNum: number) => {
+  const generateColumn = (colNum: number) => {
+    const elements = [];
+    for (let i = 0; i < gameBoard.boardWidth; i++) {
+      elements.push(<td key={`${i} - ${colNum}`} className="grid-cell"></td>);
+    }
+    return elements;
+  };
+
+  const generateRows = () => {
     const elements = [];
     for (let i = 0; i < gameBoard.boardHeight; i++) {
-      elements.push(<td key={`${i} - ${rowNum}`} className="grid-cell"></td>);
+      elements.push(
+        <tr>
+          {generateColumn(i)}
+        </tr>,
+      );
     }
     return elements;
   };
@@ -21,36 +33,7 @@ const App = () => {
       <p>Finding game, place your ships</p>
       <table id="game-board">
         <tbody className="grid-container">
-          <tr>
-            {generateRow(0)}
-          </tr>
-          <tr>
-            {generateRow(1)}
-          </tr>
-          <tr>
-            {generateRow(2)}
-          </tr>
-          <tr>
-            {generateRow(3)}
-          </tr>
-          <tr>
-            {generateRow(4)}
-          </tr>
-          <tr>
-            {generateRow(5)}
-          </tr>
-          <tr>
-            {generateRow(6)}
-          </tr>
-          <tr>
-            {generateRow(7)}
-          </tr>
-          <tr>
-            {generateRow(8)}
-          </tr>
-          <tr>
-            {generateRow(9)}
-          </tr>
+          {generateRows()}
         </tbody>
       </table>
     </div>
